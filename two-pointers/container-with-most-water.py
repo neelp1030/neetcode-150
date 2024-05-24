1f76aa465d710dbc82475e79a2b995a36b846a04
Neel Patel <neelp1030@gmail.com>
2024-05-24 15:59:44 -0400

    add solution for two-pointers/container-with-most-water.py

---
+++ two-pointers/container-with-most-water.py
@@ -0,0 +1,20 @@
+# you are given an integer array heights where heights[i] represents the height of the ith bar
+# you may choose any two bars to form a container
+# return the maximum amount of water a container can store
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            elif height[r] <= height[l]:
+                r -= 1
+            
+        return res
+
+# time complexity: O(n), at every step of the iteration we converge towards the middle by one element
+# space complexity: O(1)
